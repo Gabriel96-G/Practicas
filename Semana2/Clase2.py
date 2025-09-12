ea282b360f0a9c9736b773492c3a39e2afc6835f
Ale9624 <amedrano9624@gmail.com>
2025-09-11 20:10:07 -0600

    Prácticasemana2

---
+++ Semana2/Clase2.py
@@ -0,0 +1,22 @@
+x = 5
+y = 10
+z = x + y
+print(z)
+
+################################
+
+def suma(a, b):
+    #variable local
+    a = 10
+    b = 20
+################################
+
+saludo = "Hola"
+n = 10
+pi = 3.1416
+lista = [1, 2, 3, 4, 5]
+dos_palabras = "Hola Mundo"   #SnakeCase
+dospalabras="HolaMundo" #CamelCase
+
+################################
+
